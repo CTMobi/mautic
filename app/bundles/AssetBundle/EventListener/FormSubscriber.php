bafa63c61e2d77ce45d0cf6a505ddf8af3f16a77
Alfredo Arena <alfredo.arena@ctmobi.it>
2020-08-30 09:49:52 +0200

    Indent code

--- app/bundles/AssetBundle/EventListener/FormSubscriber.php
+++ app/bundles/AssetBundle/EventListener/FormSubscriber.php
@@ -182,7 +182,7 @@ class FormSubscriber implements EventSubscriberInterface
             '%url%' => $url,
         ]);
 
-        $analytics   = $this->analyticsHelper->getCode();
+        $analytics = $this->analyticsHelper->getCode();
 
         if (!empty($analytics)) {
             $this->assetsHelper->addCustomDeclaration($analytics);
